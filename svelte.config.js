39e518f0ee82b66eb45298202ea889d5b7ae95b0
selfint <selfint@gmail.com>
2024-05-24 19:09:44 +0300

    deploy to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
+
 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -9,12 +10,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		adapter: adapter(),
 	}
 };
 

eca29861c81a07d703df0b26645e628d7dd58aed
selfint <selfint@gmail.com>
2024-05-24 16:59:24 +0300

    get course codes from catalog (closes #1)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { parseCatalog } from '$lib/catalogParser';
+
+	let courses: string[] = [];
+
+	let textBlob: string | undefined = undefined;
+
+	async function handleSubmit(event: Event): Promise<void> {
+		if (textBlob !== undefined) {
+			courses = parseCatalog(textBlob);
+		}
+	}
+</script>
+
+<h1>Technion Course Plot</h1>
+
+{#if courses.length === 0}
+	<h2>Enter catalog text</h2>
+	<form on:submit|preventDefault={handleSubmit}>
+		<textarea bind:value={textBlob} rows="10" cols="50"></textarea>
+		<button type="submit">Submit</button>
+	</form>
+{:else}
+	<h2>Courses</h2>
+	{#each courses as course}
+		<ul>{course}</ul>
+	{/each}
+{/if}

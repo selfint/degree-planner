135b0cbb962fa0d26b4fb60e233f8471e68cdbfa
selfint <selfint@gmail.com>
2024-05-26 13:05:16 +0300

    add shadow config to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ export default {
       sans: ['Graphik', 'sans-serif'],
       serif: ['Merriweather', 'serif'],
     },
+    boxShadow: {
+      DEFAULT: '2px 2px 0 rgba(0, 0, 0, 1)',
+      lg: '8px 8px 0 rgba(0, 0, 0, 1)'
+    },
     extend: {},
   },
   plugins: [],

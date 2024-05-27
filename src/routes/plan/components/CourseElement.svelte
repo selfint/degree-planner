3d192345057e20cc40d630e3df232d1dee4ce74c
selfint <selfint@gmail.com>
2024-05-27 16:39:45 +0300

    extract CourseElement

---
+++ src/routes/plan/components/CourseElement.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { selectedGroup } from '../stores';
+
+	export let course: Course;
+	export let onClick: (course: Course) => void;
+
+	export let index = 0;
+
+	function getCourseBg(code: string, index: number, group: Group | undefined) {
+		if (group?.courses.some((course) => course.code === code)) {
+			return 'bg-indigo-900';
+		}
+
+		return index % 2 === 0 ? 'bg-dark-500' : 'bg-dark-700';
+	}
+</script>
+
+<div
+	on:mousedown|preventDefault|stopPropagation={() => onClick(course)}
+	role="button"
+	tabindex={index}
+	class="{getCourseBg(course.code, index, $selectedGroup)} p-1"
+>
+	<p class="text-sm text-white">{course.info?.name}</p>
+</div>
--- src/routes/plan/components/Semester.svelte
+++ src/routes/plan/components/Semester.svelte
@@ -1,27 +1,12 @@
 <script lang="ts">
-	import { selectedGroup } from '../stores';
+	import CourseElement from './CourseElement.svelte';
 
 	export let name: 'Winter' | 'Spring' | 'Summer';
 	export let courses: Course[];
 	export let onCourseClick: (code: string) => void;
-
-	function getCourseBg(code: string, index: number, group: Group | undefined) {
-		if (group?.courses.some((course) => course.code === code)) {
-			return 'bg-indigo-900';
-		}
-
-		return index % 2 === 0 ? 'bg-dark-500' : 'bg-dark-700';
-	}
 </script>
 
 <h2 class="border-b-2 border-dark-400 pl-2 text-lg text-white">{name}</h2>
 {#each courses as course, i}
-	<div
-		on:mousedown|preventDefault|stopPropagation={() => onCourseClick(course.code)}
-		role="button"
-		tabindex={i}
-		class="{getCourseBg(course.code, i, $selectedGroup)} p-1"
-	>
-		<p class="text-sm text-white">{course.info?.name}</p>
-	</div>
+	<CourseElement {course} index={i} onClick={(c) => onCourseClick(c.code)} />
 {/each}

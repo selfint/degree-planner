d044246a3049a5526d1e6d49b173bfd3af623425
selfint <selfint@gmail.com>
2024-05-27 12:46:40 +0300

    extract wishlist component

--- src/routes/plan/+page.svelte
+++ src/routes/plan/+page.svelte
@@ -6,6 +6,7 @@
 	import { get, writable } from 'svelte/store';
 	import Year from './Year.svelte';
 	import { selectedSemester } from './stores';
+	import Wishlist from './Wishlist.svelte';
 
 	$: fullCourses = new Map($courses.map((course) => [course.code, course]));
 	function getFullCourse(code: string): Course {
@@ -45,14 +46,14 @@
 		$wishlist = [...new Set([...$wishlist, course.code])];
 	}
 
-	function onWishlistClick(course: Course): void {
+	function onWishlistClick(code: string, toYears: boolean): void {
 		if (toYears) {
 			if ($selectedSemester !== undefined) {
 				const [y, s] = $selectedSemester;
 				const yearStore = $years[y];
 				const year = get(yearStore);
 				const semester = [year.winter, year.spring, year.summer][s];
-				semester.push(course.code);
+				semester.push(code);
 
 				yearStore.set({
 					...year,
@@ -62,16 +63,14 @@
 				});
 				$years = $years;
 
-				$wishlist = $wishlist.filter((w) => w !== course.code);
+				$wishlist = $wishlist.filter((w) => w !== code);
 			}
 		} else {
-			$wishlist = $wishlist.filter((w) => w !== course.code);
+			$wishlist = $wishlist.filter((w) => w !== code);
 		}
 	}
 
 	let newYearName: string | undefined = undefined;
-
-	let toYears = true;
 
 	function deleteYear(i: number): void {
 		$years = $years.filter((_, index) => index !== i);
@@ -159,34 +158,7 @@
 		</div>
 	</div>
 	<div class="p-2">
-		<div class="flex flex-row items-center">
-			{#if !toYears}
-				<h2 class="text-xl text-white">Wishlist</h2>
-				<div class="min-w-1 flex-grow"></div>
-			{/if}
-			<button
-				class="h-7 border-2 border-dark-400 bg-teal-800 pb-1 pl-1 pr-1 text-white"
-				on:mousedown|preventDefault={() => (toYears = !toYears)}
-			>
-				{toYears ? '<--' : '-->'}
-			</button>
-			{#if toYears}
-				<div class="min-w-1 flex-grow"></div>
-				<h2 class="text-xl text-white">Wishlist</h2>
-			{/if}
-		</div>
-		{#each wishlistCourses as course, i}
-			{#if course.info !== undefined}
-				<div
-					class="{i % 2 === 0 ? 'bg-dark-500' : 'bg-dark-700'} p-1 text-sm"
-					role="button"
-					tabindex={i}
-					on:mousedown={() => onWishlistClick(course)}
-				>
-					<p class="text-white">{course.info.name}</p>
-				</div>
-			{/if}
-		{/each}
+		<Wishlist wishlist={$wishlist.map(getFullCourse)} {onWishlistClick} />
 	</div>
 	<div class="p-2">
 		<h2 class="text-xl text-white">Catalog</h2>

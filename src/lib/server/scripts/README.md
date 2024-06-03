1a5168b51a557f4d46c53649f9562d04aa633d39
Tom Selfin <selfint@gmail.com>
2024-06-03 18:06:29 +0300

    add server db builder script

---
+++ src/lib/server/scripts/README.md
@@ -0,0 +1,3 @@
+# Server scripts
+
+This directory contains automation scripts that are not part of the deployed server.
---
+++ src/lib/server/scripts/lib/catalogParser.test.ts
@@ -0,0 +1,49 @@
+import { describe, it, expect } from 'vitest';
+
+import { parseCatalog } from './catalogParser';
+
+describe('Catalog Parser', () => {
+	it('parses 5 digit courses', () => {
+		expect(parseCatalog('12345')).toStrictEqual(['12345']);
+	});
+
+	it('removes duplicates', () => {
+		const text = '123456 123456';
+		const codes = ['123456'];
+
+		expect(parseCatalog(text)).toStrictEqual(codes);
+	});
+
+	it('parses correctly', () => {
+		const text = `'קנ
+    236306 םיירקמ םיפרג 2.0
+    236309 הניפצה תרותל אובמ 3.0
+    236313 תויכוביסה תרות 3.0
+    236315 בשחמה יעדמב תוירבגלא תוטיש 3.0
+    236318 תוינאילוב תויצקנופ לש הזילנא 2.0
+    236359 םימתירוגלא 2 3.0
+    236374 םימתירוגלאו תויתורבתסה תוטיש 3.0
+    236377 םיפרגב םירזובמ םימתירוגלא 3.0
+    236378 לוהינ תונורקע תואדו רסח עדימ 2.0
+    236508 תויכוביסו היפרגוטפירק 2.0
+    236518 תרושקת תויכוביס 2.0
+    236521 ק ימתירוגלא י בור 2.0
+    236525`;
+		const codes = [
+			'236306',
+			'236309',
+			'236313',
+			'236315',
+			'236318',
+			'236359',
+			'236374',
+			'236377',
+			'236378',
+			'236508',
+			'236518',
+			'236521',
+			'236525'
+		];
+		expect(parseCatalog(text)).toStrictEqual(codes);
+	});
+});

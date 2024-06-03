f1919a342520c48649dede20dc2bd9749191c720
Tom Selfin <selfint@gmail.com>
2024-06-03 18:06:48 +0300

    remove old website

--- src/lib/api.ts
+++
@@ -1,9 +0,0 @@
-export async function getCourseInfo(course: string): Promise<CourseInfo | undefined> {
-	const response = await fetch('/api/courseInfo' + '?c=' + course);
-
-	if (response.ok) {
-		return await response.json();
-	} else {
-		return undefined;
-	}
-}

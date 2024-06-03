3721952ef151b42c85873df48f58b6ffc27dc154
Tom Selfin <selfint@gmail.com>
2024-06-03 18:06:48 +0300

    remove old website

--- src/routes/plan/stores.ts
+++
@@ -1,4 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const selectedSemester = writable<[number, number] | undefined>(undefined);
-export const selectedGroup = writable<Group | undefined>(undefined);

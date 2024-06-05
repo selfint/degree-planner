e79b50cc88ef7940e819805eb745b529b7a95f1e
Tom Selfin <selfint@gmail.com>
2024-06-05 18:28:27 +0300

    implement degree selection

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store';
 
 export const username = writable<string | undefined>(undefined);
-export const degree = writable<string | undefined>(undefined);
+export const degree = writable<[string, string] | undefined>(undefined);
 export const plan = writable<string[][] | undefined>(undefined);
 export const currentSemester = writable<number | undefined>(undefined);
--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -1,18 +1,9 @@
 <script lang="ts">
 	import { username, degree } from '$lib/stores';
 
-	import Button from '$lib/components/Button.svelte';
-	import Select from '$lib/components/Select.svelte';
+	import Degree from './components/Degree.svelte';
 
 	$username = $username ?? 'guest';
-
-	let degreeChoice = $degree;
-	const degrees = [
-		'Computer Science',
-		'Electrical Engineering',
-		'Biotechnology',
-		'Material Engineering'
-	];
 </script>
 
 <div class="flex flex-col space-y-8">
@@ -21,20 +12,6 @@
 	</h1>
 
 	<div class="flex flex-col space-y-3">
-		<h2 class="text-xl text-content-primary">Degree</h2>
-		<div>
-			<Select bind:value={degreeChoice}>
-				{#if $degree === undefined && degreeChoice === undefined}
-					<option value={undefined}>Select a degree</option>
-				{/if}
-				{#each degrees as degree}
-					<option value={degree}>{degree}</option>
-				{/each}
-			</Select>
-			{#if degreeChoice !== $degree}
-				<Button variant="primary" onClick={() => ($degree = degreeChoice)}>Save</Button>
-				<Button variant="secondary" onClick={() => (degreeChoice = $degree)}>Cancel</Button>
-			{/if}
-		</div>
+		<Degree {degree} />
 	</div>
 </div>

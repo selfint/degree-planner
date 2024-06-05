a4b087c6d0a5b9aa78f57f9703f60c8c77598dff
Tom Selfin <selfint@gmail.com>
2024-06-05 16:12:06 +0300

    extract Select component

---
+++ src/lib/components/Select.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let value: string | undefined;
+</script>
+
+<select
+	bind:value
+	class="w-fit rounded-md border border-border bg-background p-2 text-content-secondary outline-none"
+>
+	<slot />
+</select>

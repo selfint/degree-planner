8ccf85cd77d37b106cc63d5a1c20cc9d56bf50fb
Tom Selfin <selfint@gmail.com>
2024-06-05 16:13:18 +0300

    add basic GET api routes

---
+++ src/routes/api/catalog/[year]/[faculty]/[plan]/+server.ts
@@ -0,0 +1,21 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+import { json, error } from '@sveltejs/kit';
+
+const DB_PATH = path.resolve(process.cwd(), 'static', '_db');
+
+export const GET = async ({ params: { year, faculty, plan } }) => {
+	const facultyPath = path.join(DB_PATH, year, faculty, plan);
+
+	try {
+		return json(await fs.readdir(facultyPath));
+	} catch (e) {
+		// @ts-expect-error
+		if (e.code === 'ENOENT') {
+			return error(404, 'Year not found');
+		}
+
+		return error(500, 'Internal server error');
+	}
+};

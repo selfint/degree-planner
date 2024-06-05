4745c9d74c8421983861ddb44963490d73c45288
Tom Selfin <selfint@gmail.com>
2024-06-05 12:33:48 +0300

    extract TitleBar component

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,13 +1,9 @@
 <script lang="ts">
 	export let onClick: () => void;
 	export let variant: 'primary' | 'secondary';
+	const bg = variant === 'primary' ? 'bg-accent-primary' : 'bg-card-secondary';
 </script>
 
-<button
-	class="rounded-md p-2 bg-{variant === 'primary'
-		? 'accent-primary'
-		: 'accent-secondary'} text-content-primary"
-	on:click={onClick}
->
+<button class="rounded-md p-2 pl-3 pr-3 {bg} text-content-primary" on:click={onClick}>
 	<slot />
 </button>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,49 +3,12 @@
 
 	import '../app.css';
 
-	import { page } from '$app/stores';
-
-	import Logo from '$lib/assets/logo.png';
-	import Search from '$lib/components/Search.svelte';
-	import Button from '$lib/components/Button.svelte';
+	import TitleBar from '$lib/components/TitleBar.svelte';
 
 	injectSpeedInsights();
-
-	$: borders = new Map<string, string>(
-		['catalog', 'overview', 'semester'].map((path) => [
-			path,
-			'/' + path === $page.url.pathname ? 'border-b-2 border-accent-primary' : ''
-		])
-	);
 </script>
 
-<header class="flex flex-row items-center border-b-2 border-dark-400 p-2">
-	<a href="/" class="flex flex-row items-end">
-		<img src={Logo} alt="Logo" class="h-12" />
-	</a>
-	<nav class="flex flex-grow flex-row items-baseline space-x-8">
-		<a
-			href="/"
-			class="border-b-2 border-background text-4xl font-semibold tracking-tight text-content-primary"
-		>
-			Degree Planner
-		</a>
-		<a href="/catalog" class="{borders.get('catalog')} text-2xl font-thin text-content-primary">
-			Catalog
-		</a>
-		<a href="/overview" class="{borders.get('overview')} text-2xl font-thin text-content-primary">
-			Overview
-		</a>
-		<a href="/semester" class="{borders.get('semester')} text-2xl font-thin text-content-primary">
-			Semester
-		</a>
-		<Search onSearch={() => {}} />
-	</nav>
-	<div class="flex-grow" />
-	<div>
-		<Button variant="primary" onClick={() => {}}>Get started</Button>
-	</div>
-</header>
+<TitleBar />
 
 <slot />
 

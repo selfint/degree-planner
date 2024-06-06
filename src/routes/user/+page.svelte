1e294ce7586a265355423f79de5ff857f941b746
Tom Selfin <selfint@gmail.com>
2024-06-06 12:36:01 +0300

    get degree requirements

--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -4,6 +4,11 @@
 	import Degree from './components/Degree.svelte';
 
 	$username = $username ?? 'guest';
+
+	$: requirements =
+		$degree === undefined
+			? undefined
+			: fetch(`/api/catalog/${$degree[0]}/${$degree[1]}/${$degree[2]}`).then((res) => res.json());
 </script>
 
 <div class="flex flex-col space-y-8">
@@ -14,4 +19,17 @@
 	<div class="flex flex-col space-y-3">
 		<Degree bind:degree={$degree} />
 	</div>
+
+	{#if requirements !== undefined}
+		<div class="flex flex-col space-y-3">
+			<h2 class="text-xl font-medium text-content-primary">Requirements</h2>
+			<span class="text-content-secondary">
+				{#await requirements}
+					<progress />
+				{:then requirements}
+					{JSON.stringify(requirements)}
+				{/await}
+			</span>
+		</div>
+	{/if}
 </div>

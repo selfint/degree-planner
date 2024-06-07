180a7a14689475e0c168562f16eb194dd2c2bd02
Tom Selfin <selfint@gmail.com>
2024-06-07 18:26:22 +0300

    parse degree data

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,17 +1,7 @@
-import type { Writable } from 'svelte/store';
-import { writable, derived } from 'svelte/store';
-import manifest from '$lib/assets/manifest.json';
+import { writable } from 'svelte/store';
 
 export const username = writable<string | undefined>(undefined);
 export const degree = writable<Degree | undefined>(undefined);
-export const requirements = derived<Writable<Degree | undefined>, Requirements | undefined>(
-	degree,
-	($degree) => {
-		if ($degree === undefined) return undefined;
-
-		// @ts-expect-error
-		return manifest[$degree[0]][$degree[1]][$degree[2]].requirements;
-	}
-);
 export const semesters = writable<string[][] | undefined>(undefined);
 export const currentSemester = writable<number | undefined>(undefined);
+export const degreeData = writable<Promise<DegreeData> | undefined>(undefined);

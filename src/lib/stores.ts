f335e681eb15d7e726fe95cc83c71a162deccadb
Tom Selfin <selfint@gmail.com>
2024-06-07 18:33:53 +0300

    add courseData store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -5,3 +5,6 @@ export const degree = writable<Degree | undefined>(undefined);
 export const semesters = writable<string[][] | undefined>(undefined);
 export const currentSemester = writable<number | undefined>(undefined);
 export const degreeData = writable<Promise<DegreeData> | undefined>(undefined);
+export const courseData = writable<
+	Map<string, Promise<Course | undefined>> | undefined
+>(undefined);

7adabab6786ea465b5c0fce0115bdc8ce7a70de2
Tom Selfin <selfint@gmail.com>
2024-06-08 23:44:03 +0300

    add semester names

--- src/routes/semester/+page.svelte
+++ src/routes/semester/+page.svelte
@@ -1,24 +1,63 @@
 <script lang="ts">
 	import { semesters, getCourseData, degreeData } from '$lib/stores';
 
-	import Course from '$lib/components/Course.svelte';
+	import CourseElement from '$lib/components/CourseElement.svelte';
 	import { getCourseLists } from '$lib/requirements';
+
+	function getAvgMedian(courses: Course[]): number {
+		// @ts-expect-error
+		const medians: number[] = courses
+			.map((c) => c.median)
+			.filter((m) => m !== undefined);
+
+		console.log(medians);
+		return medians.length > 0
+			? Math.round(medians.reduce((a, b) => a + b) / medians.length)
+			: 0;
+	}
 </script>
 
 <div class="flex flex-row space-x-4">
-	{#each $semesters as semester}
-		<div class="flex flex-col space-y-2">
-			{#each semester as code}
-				{#await getCourseData(code) then data}
-					<Course
-						{code}
-						{data}
-						lists={$degreeData?.then((d) =>
-							getCourseLists(d.requirements, code)
-						)}
-					/>
-				{/await}
-			{/each}
+	{#each $semesters as semester, i}
+		<div class="w-56 space-y-2">
+			<div class="flex flex-row items-baseline justify-between">
+				<h1 class="text-2xl font-medium text-content-primary">
+					{['Winter', 'Spring', 'Summer'][i % 3]}
+					{Math.floor(i / 3) + 1}
+				</h1>
+
+				<div
+					class="flex flex-row items-baseline justify-end space-x-1 text-content-secondary"
+				>
+					<span>
+						{semester.length}
+					</span>
+					{#await Promise.all(semester.map(getCourseData))}
+						<span>_</span>
+						<span>_</span>
+					{:then data}
+						<span>
+							{getAvgMedian(data)}
+						</span>
+						<span>
+							{data.reduce((a, b) => a + (b.points ?? 0), 0)}
+						</span>
+					{/await}
+				</div>
+			</div>
+			<div class="flex flex-col space-y-2">
+				{#each semester as code}
+					{#await getCourseData(code) then data}
+						<CourseElement
+							{code}
+							{data}
+							lists={$degreeData?.then((d) =>
+								getCourseLists(d.requirements, code)
+							)}
+						/>
+					{/await}
+				{/each}
+			</div>
 		</div>
 	{/each}
 </div>

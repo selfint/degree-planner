f2cfd4504fcf6c79d536df8eae7ac992c3ab1aaf
Tom Selfin <selfint@gmail.com>
2024-06-08 16:51:41 +0300

    render total degree points progress

--- src/routes/user/components/DegreeProgressElement.svelte
+++ src/routes/user/components/DegreeProgressElement.svelte
@@ -16,6 +16,15 @@
 	{#await degreeProgress}
 		<div class="text-content-secondary">Loading...</div>
 	{:then degreeProgress}
+		<div class="flex flex-row items-center space-x-2 text-content-secondary">
+			<span>Points</span>
+			<progress
+				value={degreeProgress.points[0]}
+				max={degreeProgress.points[1]}
+			/>
+			<span>{degreeProgress.points[0]} / {degreeProgress.points[1]}</span>
+		</div>
+
 		{#each degreeProgress.requirements as [name, [requirement, progress]]}
 			<div class="flex flex-col space-y-2 pl-2">
 				<h3 class="text-content-primary">{formatName(name)}</h3>

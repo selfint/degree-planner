56195f471bab387d7d18852a0ddf6cc42e19ddd0
Tom Selfin <selfint@gmail.com>
2024-06-08 11:29:25 +0300

    add point progress check

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -5,6 +5,19 @@ export const degree = writable<Degree | undefined>(undefined);
 export const semesters = writable<string[][] | undefined>(undefined);
 export const currentSemester = writable<number | undefined>(undefined);
 export const degreeData = writable<Promise<DegreeData> | undefined>(undefined);
-export const courseData = writable<
-	Map<string, Promise<Course | undefined>> | undefined
->(undefined);
+export const degreeProgress = writable<Promise<DegreeProgress> | undefined>(
+	undefined
+);
+
+const courseData: CourseData = new Map();
+export function getCourseData(code: string): Promise<Course> {
+	const data = courseData.get(code);
+	if (data === undefined) {
+		const future = fetch(`/api/courseInfo/${code}`).then((res) => res.json());
+		courseData.set(code, future);
+
+		return future;
+	} else {
+		return data;
+	}
+}

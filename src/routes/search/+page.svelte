dd533943038b0117ec3d21f414d831c14498634b
Tom Selfin <selfint@gmail.com>
2024-06-09 20:23:59 +0300

    tweak ui

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -29,22 +29,24 @@
 	{#await searchResults}
 		<p>Loading...</p>
 	{:then results}
-		{#if results.length === 0}
-			<p>No results found</p>
-		{:else}
-			<ul class="flex flex-row flex-wrap">
-				{#each results as course}
-					<li class="pb-2 pl-1 pr-1 pt-2">
-						<CourseElement
-							{course}
-							requirements={$degreeData?.then((d) =>
-								getCourseLists(d.requirements, course.code)
-							)}
-						/>
-					</li>
-				{/each}
-			</ul>
-		{/if}
+		<h1 class="text-2xl">
+			Results for "{query}"
+		</h1>
+		<p class="mb-3 text-base text-content-secondary">
+			{results.length} results found
+		</p>
+		<ul class="flex flex-row flex-wrap">
+			{#each results as course}
+				<li class="pb-4 pr-2">
+					<CourseElement
+						{course}
+						requirements={$degreeData?.then((d) =>
+							getCourseLists(d.requirements, course.code)
+						)}
+					/>
+				</li>
+			{/each}
+		</ul>
 	{:catch error}
 		<p>Error: {error.message}</p>
 	{/await}

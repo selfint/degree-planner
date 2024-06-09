dcf9b9eb002d36bdacb54feb5458dcd0255bcc8f
Tom Selfin <selfint@gmail.com>
2024-06-09 11:46:58 +0300

    move courseData to separate file
    It isn't a store, and has a lot of custom logic

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -8,27 +8,3 @@ export const degreeData = writable<Promise<DegreeData> | undefined>(undefined);
 export const degreeProgress = writable<Promise<DegreeProgress> | undefined>(
 	undefined
 );
-
-const cacheRoute = '/_cache/courseData/';
-const courseData: CourseData = new Map();
-export function getCourseData(code: string): Promise<Course> {
-	const data = courseData.get(code);
-	if (data === undefined) {
-		// Fetch first from cache, then from server if cache fails
-		const future = fetch(cacheRoute + code + '.json')
-			.then((res) => {
-				if (res.ok) {
-					return res.json();
-				} else {
-					throw new Error('failed fetching cached course data for ' + code);
-				}
-			})
-			.catch(() => fetch(`/api/courseInfo/${code}`).then((res) => res.json()));
-
-		courseData.set(code, future);
-
-		return future;
-	} else {
-		return data;
-	}
-}

598574cc0152decc585bb1a46d78baba6a90f066
Tom Selfin <selfint@gmail.com>
2024-06-11 11:20:22 +0300

    use better hash function

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -1,14 +1,24 @@
-function hashString(str: string): number {
-	let hash = 0;
-	for (let i = 0; i < str.length; i++) {
-		hash = (hash << 5) - hash + str.charCodeAt(i);
-		hash |= 0;
+/**
+ * https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
+ */
+function cyrb53(str: string, seed = 0) {
+	let h1 = 0xdeadbeef ^ seed,
+		h2 = 0x41c6ce57 ^ seed;
+	for (let i = 0, ch; i < str.length; i++) {
+		ch = str.charCodeAt(i);
+		h1 = Math.imul(h1 ^ ch, 2654435761);
+		h2 = Math.imul(h2 ^ ch, 1597334677);
 	}
-	return hash;
+	h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+	h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+	h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+	h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+
+	return 4294967296 * (2097151 & h2) + (h1 >>> 0);
 }
 
 export function generateColor(source: string): string {
-	const hash = hashString(source.repeat(11));
+	const hash = cyrb53(source);
 	const hue = hash % 360;
 	const saturation = 50;
 	const lightness = 30;

0ca0cb44f4137e5f74047babd6d3309d81afee89
Tom Selfin <selfint@gmail.com>
2024-06-14 16:45:35 +0300

    fix schedule error logic

--- src/routes/overview/components/ScheduleError.svelte
+++ src/routes/overview/components/ScheduleError.svelte
@@ -9,61 +9,51 @@
 	const previous = semesters.slice(0, index);
 
 	let dep: Promise<Course[][]> = (async () => [])();
-	let adj: Promise<Course[]> = (async () => [])();
+	let adjacency: Promise<Course[]> = (async () => [])();
 
-	function dependencyTaken(code: string): boolean {
-		return previous.flat().includes(code);
+	function dependencyTaken(course: Course): boolean {
+		return previous.flat().includes(course.code);
 	}
 
-	function adjacencyTaken(code: string): boolean {
-		return dependencyTaken(code) || semester.includes(code);
+	function adjacencyTaken(course: Course): boolean {
+		return dependencyTaken(course) || semester.includes(course.code);
 	}
 
-	if (course.connections !== undefined) {
-		const dependencies = course.connections.dependencies ?? [];
-		const adjacencies = course.connections.adjacent ?? [];
+	const dependencies = Promise.all(
+		(course.connections?.dependencies ?? []).map(
+			async (group) => await Promise.all(group.map(getCourseData))
+		)
+	).then((dep) =>
+		dep
+			.map((g) => g.filter((c) => (c.name ?? '').includes('-')))
+			.filter((g) => g.length > 0)
+	);
+	const adjacencies = Promise.all(
+		(course.connections?.adjacent ?? []).map(getCourseData)
+	).then((adj) => adj.filter((c) => (c.name ?? '').includes('-')));
 
-		const dependenciesSatisfied =
-			dependencies.length === 0 ||
-			dependencies.some((group) => group.every(dependencyTaken));
+	const dependenciesSatisfied = (dependencies: Course[][]) =>
+		dependencies.length === 0 ||
+		dependencies.some((group) => group.every(dependencyTaken));
 
-		const adjacenciesSatisfied =
-			adjacencies.length === 0 || adjacencies.some(adjacencyTaken);
-
-		if (!dependenciesSatisfied) {
-			dep = Promise.all(
-				dependencies.map(
-					async (group) => await Promise.all(group.map(getCourseData))
-				)
-			).then((dep) =>
-				dep
-					.map((g) => g.filter((c) => (c.name ?? '').includes('-')))
-					.filter((g) => g.length > 0)
-			);
-		}
-
-		if (!adjacenciesSatisfied) {
-			adj = Promise.all(adjacencies.map(getCourseData)).then((adj) =>
-				adj.filter((c) => (c.name ?? '').includes('-'))
-			);
-		}
-	}
+	const adjacenciesSatisfied = (adjacencies: Course[]) =>
+		adjacencies.length === 0 || adjacencies.some(adjacencyTaken);
 </script>
 
-{#await dep}
+{#await dependencies}
 	<p class="p-2 pb-1 pt-1 text-content-secondary">Loading...</p>
-{:then dep}
-	{#if dep.length > 0}
+{:then dependencies}
+	{#if !dependenciesSatisfied(dependencies)}
 		<div class="p-2 pb-1 pt-1">
 			<h2 class="text-base text-content-primary">Dependencies</h2>
 			<div class="mb-2 space-y-2">
-				{#each dep as group, i}
+				{#each dependencies as group, i}
 					{#if i !== 0}
 						<p class="w-full text-center text-content-secondary">OR</p>
 					{/if}
 					<div class="space-y-1">
 						{#each group as course}
-							<slot name="dep" {course} taken={dependencyTaken(course.code)} />
+							<slot name="dep" {course} taken={dependencyTaken(course)} />
 						{/each}
 					</div>
 				{/each}
@@ -72,15 +62,15 @@
 	{/if}
 {/await}
 
-{#await adj}
+{#await adjacencies}
 	<p class="p-2 pb-1 pt-1 text-content-secondary">Loading...</p>
-{:then adj}
-	{#if adj.length > 0}
+{:then adjacencies}
+	{#if !adjacenciesSatisfied(adjacencies)}
 		<div class="p-2 pb-1 pt-1">
 			<h2 class="text-base text-content-primary">Adjacencies</h2>
 			<div class="space-y-1">
-				{#each adj as course}
-					<slot name="adj" {course} taken={adjacencyTaken(course.code)} />
+				{#each adjacencies as course}
+					<slot name="adj" {course} taken={adjacencyTaken(course)} />
 				{/each}
 			</div>
 		</div>

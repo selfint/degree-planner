1ab57e1f30ef1ada7ebb8fb600900f2cca5dd6b3
Tom Selfin <selfint@gmail.com>
2024-08-19 13:21:54 +0300

    add option to change total semesters

--- src/routes/user/+page.svelte
+++ src/routes/user/+page.svelte
@@ -54,6 +54,20 @@
 	}
 
 	let semesterChoice = $currentSemester;
+	let totalSemestersChoice = $semesters.length;
+
+	const maxNonEmptySemesterIndex =
+		$semesters
+			.map((s, i) => [s.length, i])
+			.filter(([s]) => s > 0)
+			.map(([, i]) => i)
+			// get the last non-empty semester
+			.reduce((a, b) => Math.max(a, b), 0) + 1;
+	const maxTotalSemesters = 15;
+	const validTotalValues = Array.from(
+		{ length: maxTotalSemesters },
+		(_, i) => i + 1
+	).filter((i) => i >= maxNonEmptySemesterIndex);
 </script>
 
 <div class="m-3 flex flex-col space-y-8">
@@ -91,6 +105,43 @@
 					<Button
 						variant="secondary"
 						onClick={() => (semesterChoice = $currentSemester)}
+					>
+						Cancel
+					</Button>
+				{/if}
+			</div>
+			<div class="flex flex-row items-baseline space-x-3">
+				<span class="text-content-secondary"> Total: </span>
+				<Select bind:value={totalSemestersChoice}>
+					{#each validTotalValues as i}
+						<option value={i}>
+							{i}
+							({['Winter', 'Spring', 'Summer'][(i - 1) % 3]}
+							{Math.floor((i - 1) / 3) + 1})
+						</option>
+					{/each}
+				</Select>
+
+				{#if totalSemestersChoice !== $semesters.length}
+					<Button
+						variant="primary"
+						onClick={() => {
+							if (totalSemestersChoice < $semesters.length) {
+								$semesters = $semesters.slice(0, totalSemestersChoice);
+							} else if (totalSemestersChoice > $semesters.length) {
+								$semesters = $semesters.concat(
+									Array.from({
+										length: totalSemestersChoice - $semesters.length
+									}).map(() => [])
+								);
+							}
+						}}
+					>
+						Save
+					</Button>
+					<Button
+						variant="secondary"
+						onClick={() => (totalSemestersChoice = $semesters.length)}
 					>
 						Cancel
 					</Button>

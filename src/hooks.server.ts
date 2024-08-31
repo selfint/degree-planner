2a7c0a77b5fa780b5e771d4f54d40c794413db48
Tom Selfin <selfint@gmail.com>
2024-08-31 21:04:10 +0300

    silence cache 404 errors

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -10,31 +10,10 @@ export const handleError: HandleServerError = async ({
 	status,
 	message
 }) => {
-	console.error(error);
-
-	// in dev mode, if course data is not found, create a cache file with the course code
-	// to mark it as not found
-	if (
-		import.meta.env.MODE === 'development' &&
-		status === 404 &&
-		event.request.url.includes('/_cache/courseData/')
-	) {
-		try {
-			await fs.mkdir(cacheDir, { recursive: true });
-		} catch (e) {
-			// ignore
-		}
-
-		const courseFilename = event.request.url.split('/').pop();
-
-		if (courseFilename === undefined) {
-			return;
-		}
-
-		const code = courseFilename.replace('.json', '');
-		await fs.writeFile(
-			path.join(cacheDir, courseFilename),
-			JSON.stringify({ code })
-		);
+	// ignore 404 errors for course data cache
+	if (status === 404 && event.request.url.includes('/_cache/courseData/')) {
+		return;
 	}
+
+	console.error(error);
 };

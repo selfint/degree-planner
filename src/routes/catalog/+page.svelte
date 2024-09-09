b4da49673f2842740313a817f19bec3cf278725e
Tom Selfin <selfint@gmail.com>
2024-09-09 16:52:50 +0300

    add course recommendations

--- src/routes/catalog/+page.svelte
+++ src/routes/catalog/+page.svelte
@@ -1,12 +1,58 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+
 	import CourseRow from './components/CourseRow.svelte';
+	import CourseElement from '$lib/components/CourseElement.svelte';
 
-	import { degreeData, wishlist } from '$lib/stores';
-	import { getDegreeRequirementCourses } from '$lib/requirements';
+	import { degreeData, wishlist, semesters } from '$lib/stores';
+	import {
+		getDegreeRequirementCourses,
+		getCourseLists
+	} from '$lib/requirements';
+	import { getAllCoursesSync, getCourseData } from '$lib/courseData';
+	import { bm25 } from '$lib/bm25';
 
-	$: lists = $degreeData?.then((d) =>
-		getDegreeRequirementCourses(d.requirements)
-	);
+	$: lists =
+		$degreeData?.then((d) => getDegreeRequirementCourses(d.requirements)) ?? [];
+
+	$: recommendations =
+		$degreeData?.then(async (d) => {
+			const courses = getAllCoursesSync();
+
+			const nonRecommendedCourses = await Promise.all(
+				$semesters
+					.flat()
+					.filter((c) => !d.recommended.flat().includes(c))
+					.map(async (c) => await getCourseData(c))
+			);
+
+			const queries = nonRecommendedCourses
+				.filter((c) => c.name !== undefined && (c.about?.length ?? 0) > 10)
+				.map((c) => ({
+					course: c,
+					query: c.name + ' ' + c.about
+				}));
+
+			const corpusCourses = (await courses)
+				.filter((c) => c.name !== undefined && (c.about?.length ?? 0) > 10)
+				.filter((c) => !$semesters.flat().includes(c.code));
+
+			const corpus = corpusCourses.map((c) => c.name + ' ' + c.about);
+
+			const results = queries
+				.map(({ course, query }) => ({
+					course,
+					hits: bm25(query, corpus).slice(0, 30)
+				}))
+				.toSorted((a, b) => b.hits[0].score - a.hits[0].score)
+				.slice(0, 5)
+				.map(({ course, hits }) => ({
+					course,
+					hits: hits.map(({ index }) => corpusCourses[index])
+				}));
+
+			return results;
+		}) ?? Promise.resolve([]);
 </script>
 
 <div class="m-3 mr-0">
@@ -14,10 +60,47 @@
 	{#await lists}
 		<div class="text-content-secondary">Loading...</div>
 	{:then lists}
-		{#each lists ?? [] as list}
+		{#each lists as list}
 			{#if list.courses.length > 0}
 				<CourseRow titles={list.path} codes={list.courses} />
 			{/if}
 		{/each}
+	{/await}
+
+	{#await recommendations then recommendations}
+		{#each recommendations as { course, hits }, i}
+			<div class="mb-4 min-h-[118px] max-w-full">
+				<h1
+					class="mb-2 flex flex-row items-baseline space-x-4 text-2xl font-medium text-content-primary"
+				>
+					<div class="flex flex-row items-baseline space-x-2">
+						<span class="w-fit pr-2"> More like {course.name} </span>
+					</div>
+				</h1>
+				<div class="mb-4 flex w-full flex-row space-x-2 overflow-x-auto">
+					{#each hits as course, i}
+						<div
+							class="container w-fit"
+							tabindex={i}
+							role="button"
+							on:click={() => goto(`/course/${course.code}`)}
+							on:keydown={(e) => {
+								if (e.key === 'Enter') {
+									goto(`/course/${course.code}`);
+								}
+							}}
+						>
+							<CourseElement
+								{course}
+								requirements={$degreeData?.then((d) =>
+									getCourseLists(d.requirements, course.code)
+								)}
+							/>
+						</div>
+					{/each}
+				</div>
+			</div>
+		{/each}
+		<div class="mb-4 flex w-full flex-row space-x-2 overflow-x-auto"></div>
 	{/await}
 </div>

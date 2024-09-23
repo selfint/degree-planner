f63d8a6157775d771a717a683441e7ea4de34153
Tom Selfin <selfint@gmail.com>
2024-09-23 16:27:26 +0300

    improve mobile navbar ux

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -11,7 +11,7 @@
 </script>
 
 <div
-	class="flex h-fit w-fit flex-row items-center justify-start rounded-md bg-card-primary pb-1 pl-2 pr-1 pt-1 sm:min-w-64 {textStyle}"
+	class="flex h-fit w-fit min-w-48 flex-row items-center justify-start rounded-md bg-card-primary pb-1 pl-2 pr-1 pt-1 {textStyle}"
 >
 	<SearchIcon class="h-3" />
 	<form class="ml-1 w-24" on:submit|preventDefault={() => onSearch(query)}>

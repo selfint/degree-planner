f2f6fbcc88e1cb0dd145e81dc269c5581999b652
Tom Selfin <selfint@gmail.com>
2024-10-10 17:32:37 +0300

    tweak ui

--- src/lib/components/Burger.svelte
+++ src/lib/components/Burger.svelte
@@ -5,7 +5,7 @@
 	viewBox="0 0 24 24"
 	xmlns="http://www.w3.org/2000/svg"
 >
-	<rect width="24" height="1.5" y="4" rx="1" />
-	<rect width="24" height="1.5" y="11" rx="1" />
-	<rect width="24" height="1.5" y="18" rx="1" />
+	<rect width="24" height="1" y="4" rx="1" />
+	<rect width="24" height="1" y="11" rx="1" />
+	<rect width="24" height="1" y="18" rx="1" />
 </svg>

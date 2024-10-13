fb38b9a9e3f3c7002d640df579af95ad48dcee78
Tom Selfin <selfint@gmail.com>
2024-10-13 20:28:59 +0300

    migrate CourseWidth to svelte 5

--- src/lib/components/CourseWidth.svelte
+++ src/lib/components/CourseWidth.svelte
@@ -1,3 +1,13 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	type Props = {
+		children: Snippet;
+	};
+
+	let { children }: Props = $props();
+</script>
+
 <div class="w-[29vw] min-w-[29vw] max-w-[220px] sm:min-w-[220px]">
-	<slot />
+	{@render children()}
 </div>

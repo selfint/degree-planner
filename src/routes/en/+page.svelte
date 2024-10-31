8cf64b395890ef078f839694b9b5f7d09f32591e
Tom Selfin <selfint@gmail.com>
2024-10-31 12:08:46 +0200

    add /en route to share english version

---
+++ src/routes/en/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	import { browser } from '$app/environment';
+
+	import { content } from '$lib/stores.svelte';
+	import { cms } from '$lib/content';
+
+	if (browser) {
+		localStorage.setItem('lang', 'en');
+		content.lang = cms.en;
+
+		goto('/');
+	}
+</script>

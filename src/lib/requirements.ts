91ee6ab34656a66cc40b56b6c4791d0522bb3e10
Tom Selfin <selfint@gmail.com>
2024-11-12 19:31:15 +0200

    add boilerplate for path support

--- src/lib/requirements.ts
+++ src/lib/requirements.ts
@@ -1,16 +1,5 @@
 import catalogs from '$lib/assets/catalogs.json';
 
-export function parseCatalog(text: string): string[] {
-	const regex = /\b\d{5,6}\b/g;
-	const matches = text.match(regex);
-
-	const codes = [...new Set(matches ? matches : [])];
-	return codes
-		.map((code) => code.replace(/^0+/, ''))
-		.map((code) => '0'.repeat(6 - code.length) + code)
-		.map((code) => '0' + code.slice(0, 3) + '0' + code.slice(3));
-}
-
 async function loadCourses(
 	requirementHeader: RequirementHeader,
 	_fetch: (

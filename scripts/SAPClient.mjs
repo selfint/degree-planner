a0ee0a9c1481a66c08af02321337a60a9f623413
Tom Selfin <selfint@gmail.com>
2025-01-09 12:24:57 +0200

    fix course current field

--- scripts/SAPClient.mjs
+++ scripts/SAPClient.mjs
@@ -505,7 +505,7 @@ export async function getCourseData(courses) {
 	return await requestBatch(
 		'SmObjectSet',
 		courses.map(({ Otjid, Peryr, Perid }) => ({
-			$expand: 'Exams,SmRelations,SmPrereq',
+			$expand: 'Exams,SmRelations,SmPrereq,SmOfferedPeriodSet',
 			$filter: `Otjid eq '${Otjid}' and Peryr eq '${Peryr}' and Perid eq '${Perid}'`,
 			$select: [
 				'Otjid',
@@ -516,7 +516,8 @@ export async function getCourseData(courses) {
 				'Exams',
 				'SmPrereq',
 				'SmRelations',
-				'OrgText'
+				'OrgText',
+				'SmOfferedPeriodSet'
 			].join(',')
 		})),
 		'he',

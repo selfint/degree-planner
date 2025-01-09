c4061470d1d9b0e8f9032b8b06f33d86447965b6
Tom Selfin <selfint@gmail.com>
2025-01-10 00:40:48 +0200

    add minimize to schedule error in plan page

---
+++ src/lib/components/Arrow.svelte
@@ -0,0 +1,13 @@
+<svg
+	{...$$props}
+	{...$$restProps}
+	viewBox="0 0 21 10"
+	xmlns="http://www.w3.org/2000/svg"
+	fill="currentColor"
+>
+	<path
+		fill-rule="evenodd"
+		clip-rule="evenodd"
+		d="M0.5 1.5501L2.33469 0L10.5012 6.89871L18.6652 0L20.4999 1.5501L10.5012 10L0.5 1.5501Z"
+	/>
+</svg>
